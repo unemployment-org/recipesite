b603405a033acbea9cf8b006d646537cdaf6631e
Danial Yessimov <kkaitenu@gmail.com>
2026-04-07 00:43:35 +0700

    add whitenoise for css

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Куда перенаправлять после ВХОДА (на главную)
 LOGIN_REDIRECT_URL = 'home'
 
